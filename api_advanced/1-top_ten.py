42649dad738925ccc032b10719c25458f903ffa9
Robert <r.niyonkuru@alustudent.com>
2025-10-23 21:45:45 +0200

    API ADVANCED

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
+    subreddit = "programming"
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -14,3 +15,5 @@ def top_ten(subreddit):
     posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
+
+top_ten("programming")
